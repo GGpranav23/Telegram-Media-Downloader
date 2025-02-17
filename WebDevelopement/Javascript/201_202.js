007cab6aeb57a9617d50a35596e92cfd206488bd
PranavShinde <163328592+GGpranav23@users.noreply.github.com>
2025-02-17 20:27:06 +0530

    Delete WebDevelopement/Javascript directory

--- WebDevelopement/Javascript/201_202.js
+++
@@ -1,8 +0,0 @@
-/*** Function Expression ***/
-
-// 1. Anonymous functions are function expression 
-// 2. Named function assigned to a variable is a function expression
-
-/*** Self Invoking Functions ***/
-
-// 1. IIFE are the self invoking functions 
--- WebDevelopement/Javascript/203.js
+++
@@ -1,3 +0,0 @@
-// Objects : An object is an entity(properties and method) with a certain state and behavior
-// They are written as Key: Value pairs 
-
